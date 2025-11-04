ff7eadb6b2e70eaa6cc0c36eb863f9cef3b81f7d
vasudhatulasi <vasudhatulasi4@gmail.com>
2025-11-04 14:47:28 +0530

    Force add .env

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:5000/api',
+    baseURL: 'https://stroy-1.onrender.com',
     headers: {
         'Content-Type': 'application/json'
     }
